50ae921d5214cf6c944a92b8165b70b4554e639c
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-01 16:16:58 -0500

    Update arrayreview240.py

--- Array/arrayreview240.py
+++ Array/arrayreview240.py
@@ -12,3 +12,39 @@ def SearchMatrix(matrix, target):
         else:
             c -= 1
     return False
+
+# method 2, binary search method
+class Solution:
+    def helper(self, matrix, target, start_x, start_y, rows, cols):
+        if not matrix: return False
+        if rows <=0 or col <=0:
+            return False
+        mid_x = start_x +rows // 2
+        mid_y = start_y + cols// 2
+        if matrix[mid_x][mid_y] == target:
+            return True
+        elif rows == 1 and cols ==1:
+            return False
+        elif matrix[mid_x][mid_y] < target:
+            if self.helper(matrix, target, start_x,strat_y, rows//2, cols-cols//2):
+                return True
+            if self.helper(matrix, target, start_x,strat_y, rows-rows//2, cols//2):
+                return True
+            if self.helper(matrix, target, start_x,strat_y, rows-rows//2, cols-cols//2):
+                return True
+        else:
+            if self.helper(matrix, target, start_x,strat_y, rows//2, cols-cols//2):
+                return True
+            if self.helper(matrix, target, start_x,strat_y, rows-rows//2, cols//2):
+                return True
+            if self.helper(matrix, target, start_x,strat_y, rows//2, cols//2):
+                return True
+        return False
+    
+    def searchMatrix(self, matrix, target):
+        if not matrix: return False
+        start_x, start_y = 0, 0
+        rows, cols = len(matrix), len(matrix[0])
+        return self.helper(matrix, target, start_x, start_y, rows, cols)
+        
+        
