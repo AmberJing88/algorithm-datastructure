487f52bf38877d7f85079c944db0bf626e4a57eb
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-05 20:42:44 -0500

    Add files via upload

--- stack-queue/stack-queueReview.py
+++ stack-queue/stack-queueReview.py
@@ -29,3 +29,58 @@ class MyQueue:
 
     def empty(self):
         return self._size ==0
+
+#225
+"""225: implement stack operations using queues"""
+from queue import Queue
+class MyStack:
+    def __init__(self):
+        self.stack = Queue()
+        self._size = 0
+
+    def push(self, x):
+        reverse = Queue()
+        while not self.stack.empty():
+            reverse.put(self.stack.get())
+        self.stack.put(x)
+        while not reverse.empty():
+            self.stack.put(reverse.get())
+        self._size += 1
+
+    def pop(self):
+        if not self.empty():
+            self._size -= 1
+            return self.stack.get()
+
+    def top(self):
+        top = self.stack.get()
+        self.push(top)
+        return top
+    def empty(self):
+        return self.stack.empty()
+
+#155
+"""155: min stack design a stach that supports push, pop, top and retrieving the minimum
+element in constant time."""
+class MinStack:
+    def __init__(self):
+        self.minstack = []
+        self._size = 0
+
+    def push(self, x):
+        self.minstack.append(x)
+        self._size += 1
+
+    def pop(self):
+        self.minstack.pop()
+        self._size -= 1
+
+    def top(self):
+        if self._size ==0:
+            return False
+        else:
+            return self.minstack[-1]
+
+    def getMin(self):
+        if self._size != 0:
+            return min(self.minstack)
