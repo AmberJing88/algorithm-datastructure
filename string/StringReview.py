dc440751167a2781593522d5ec967f7c041eae2d
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-09 12:21:02 -0500

    Update StringReview.py

--- string/StringReview.py
+++ string/StringReview.py
@@ -136,7 +136,7 @@ def countsubstring(s):
             if i < right:
                 Z[i] = min(right-i, Z[2*center-i])
             while A[i+ Z[i]+1] == A[i-Z[i]-1]:
-                z[i] += 1
+                Z[i] += 1
             if i + Z[i] > right:
                 center, right = i, i+Z[i]
         return Z
