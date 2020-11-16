a833c1818329abc2795d0bfb081dd2836024f733
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-16 12:05:33 -0500

    Update BackTrackingReview44.py

--- backtracking/BackTrackingReview44.py
+++ backtracking/BackTrackingReview44.py
@@ -77,7 +77,7 @@ def isMatch(s, p):
     dp = [[False for _ in range(len(p)+1)] for _ in range(len(s)+1)]
     for i in range(len(s)+1):
         for j in range(len(p)+1):
-            star_s, start_p = i-1,j-1
+            start_s, start_p = i-1,j-1
             if i==0 and j==0:
                 dp[i][j] = True
             elif i==0:
@@ -86,7 +86,7 @@ def isMatch(s, p):
                 dp[i][j] = False
             elif p[start_p]=='*':
                 dp[i][j] = dp[i][j-1] or dp[i-1][j]
-            elif s[start_s]==p[start_p] or s[start_p]=='?':
+            elif s[start_s]==p[start_p] or p[start_p]=='?':
                 dp[i][j] = dp[i-1][j-1]
     return dp[len(s)][len(p)]
 
