4d9d3897e493d99ffc1fd5f8aad5eb33060c70be
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-18 17:48:03 -0500

    Update BackTrackingReview1091.py

--- backtracking/BackTrackingReview1091.py
+++ backtracking/BackTrackingReview1091.py
@@ -82,7 +82,7 @@ class PriorityQueue:
             heappush(self.heap,(0,value))
 
     def add(self,value,priority=0):
-        heappush(self.heap,(priority, value))
+        heappush(self,heap,(priority, value))
 
     def pop(self):
         priority,value = heappop(self.heap)
