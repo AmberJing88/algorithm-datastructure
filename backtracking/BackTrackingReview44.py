1023caf76b69d52e6c5417666d63a440da9f9453
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-21 15:31:42 -0500

    Update BackTrackingReview44.py

--- backtracking/BackTrackingReview44.py
+++ backtracking/BackTrackingReview44.py
@@ -189,7 +189,7 @@ def binarytree(root):
 # iteration stack
 def binarytree(root):
     if not root: return []
-    stack = [root]
+    stack = [(root,'')]
     while stack:
         node,ls = stack.pop()
         if not node.left and not node.right:
