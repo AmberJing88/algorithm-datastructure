f27bd0fb962b548f4caf7997d82a2f6dc6b44de2
AmberXu <46754578+AmberJing88@users.noreply.github.com>
2020-11-22 17:28:30 -0500

    Update DPReview583.py

--- dp/DPReview583.py
+++ dp/DPReview583.py
@@ -7,7 +7,7 @@ def minDistance(word1,word2):
     if n==0: return m
     dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
     for i in range(1, m+1):
-        for j in range(1,m+1):
+        for j in range(1,n+1):
             if word1[i-1]==word2[j-1]:
                 dp[i][j] = dp[i-1][j-1]+1
             else:
